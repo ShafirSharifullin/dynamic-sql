c5f368fe96b2641b13e62be9dcc9f97cce09b107
ArtemSharifullin <sharifullin@siberian.pro>
2022-08-24 17:16:30 +0700

    env: add querydsl dependencies for integration with spring data jdbc

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.3"
     id("io.spring.dependency-management") version "1.0.13.RELEASE"
+    kotlin("kapt") version "1.7.10"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
 }
@@ -19,12 +20,25 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // QueryDsl
+    implementation("com.querydsl:querydsl-sql-spring")
+    implementation("com.infobip:infobip-spring-data-jdbc-querydsl-boot-starter:7.1.0")
+    kapt("com.infobip:infobip-spring-data-jdbc-querydsl-boot-starter:7.1.0")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.flywaydb:flyway-core")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+sourceSets.main {
+    java.srcDirs(
+        "build/generated/sources/annotationProcessor/kotlin/main",
+        "build/generated/sources/annotationProcessor/kotlin/querydsl"
+    )
 }
 
 tasks.withType<KotlinCompile> {
